b8ed91cf137bdbb77bf32491e83969dd7f4ddd8f
KCSquad7201 <116947309+KCSquad7201@users.noreply.github.com>
2022-10-31 22:28:05 +0530

    Update Heart disease detection.py

--- Heart disease detection.py
+++ Heart disease detection.py
@@ -30,3 +30,22 @@ reg2.fit(X_poly, Y)
 print(reg.predict([[43,0,0,132,341,1,0,136,1,3,1,0,3]]))
 print(reg2.predict([[43,0,0,132,341,1,0,136,1,3,1,0,3]]))
 
+...
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+	results.append(cv_results)
+	names.append(name)
+	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
